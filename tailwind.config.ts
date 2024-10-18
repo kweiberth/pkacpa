be40b1ec5a8edea2acf7d13cc3df867d10f36472
Kurt Weiberth <kurt.weiberth@gmail.com>
2024-10-18 16:50:11 -0400

    Add comment re: custom breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,10 @@ const config = {
         },
       },
       screens: {
+        // 1024px is the same as Tailwind's built-in 'lg' breakpoint, but we
+        // define it here so we can easily maintain a single reference to the
+        // breakpoint @ which we switch b/w desktop nav header and mobile
+        // hamburger menu.
         menu: '1024px',
       },
     },
