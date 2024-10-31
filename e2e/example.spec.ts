99470c310916db3cf18b8bf5a19cd96b090fef7c
Kurt Weiberth <kurt.weiberth@gmail.com>
2024-10-30 21:17:44 -0400

    Update test assertion

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('Page has title', async ({ page }) => {
   await page.goto('/');
-  await expect(page).toHaveTitle('Palermo/Kissinger & Associates, P.C.');
+  await expect(page).toHaveTitle('Palermo/Kissinger & Associates');
 });
